88bd7d96a1c1e3e53a65d270c9f031f924d674f7
Bacha1234 <shassanbshassanb@gmail.com>
2025-07-19 02:25:12 -0700

    solve the anchor error on small screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,17 @@ function Navbar() {
   return cleanUp;
  },[])
 
+ const handleLinkClick = () => {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+
+  // Collapse only if it is currently shown
+  if (navbarCollapse.classList.contains('show')) {
+    navbarToggler.click(); // triggers Bootstrap's collapse
+  }
+};
+
+
   return (
   <header className="header px-md-5 px-0">
   <nav id="myNavbar" className="navbar navbar-expand-lg fixed-top">
@@ -25,25 +36,25 @@ function Navbar() {
       >
         <ul className="navbar-nav d-flex align-items-center css-breadcrumb-nav gap-5">
           <li className="nav-item">
-            <a href="#work" className="nav-link active css-nav-link">
+            <a href="#work" className="nav-link active css-nav-link" onClick={handleLinkClick}>
               Agency
             </a>
           </li>
           <li className="nav-item dropdown">
             <a href="#services"
               className="nav-link dropdown-toggle css-nav-link d-inline-flex align-items-center"
-              role="button"
+              role="button" onClick={handleLinkClick}
             >
               Services<span className="breadcrumb-separator ms-1">›</span>
             </a>
             <ul className="dropdown-menu">
               <li>
-                <a href="#services" className="dropdown-item">
+                <a href="#services" className="dropdown-item" onClick={handleLinkClick}>
                   Graphic Designing
                 </a>
               </li>
               <li>
-                <a href="#services" className="dropdown-item">
+                <a href="#services" className="dropdown-item" onClick={handleLinkClick}>
                   UI/UX
                 </a>
               </li>
@@ -52,25 +63,25 @@ function Navbar() {
           <li className="nav-item dropdown">
             <a href="#packages"
               className="nav-link dropdown-toggle css-nav-link d-inline-flex align-items-center"
-              role="button"
+              role="button" onClick={handleLinkClick}
             >
               Packages<span className="breadcrumb-separator ms-1">›</span>
             </a>
             <ul className="dropdown-menu">
               <li>
-                <a href="#packages" className="dropdown-item" >
+                <a href="#packages" className="dropdown-item" onClick={handleLinkClick}>
                   Starter
                 </a>
               </li>
               <li>
-                <a href="#packages" className="dropdown-item" >
+                <a href="#packages" className="dropdown-item" onClick={handleLinkClick} >
                   Business
                 </a>
               </li>
             </ul>
           </li>
           <li className="nav-item">
-            <a className="nav-link css-nav-link" href="#mockup">
+            <a className="nav-link css-nav-link" href="#mockup" onClick={handleLinkClick}>
               Mockups
             </a>
           </li>
@@ -78,13 +89,13 @@ function Navbar() {
         <div>
           <ul className="navbar-nav gap-3">
             <li className="nav-item">
-              <a href="#work" className="btn portfolio-link">
+              <a href="#work" className="btn portfolio-link" onClick={handleLinkClick}>
                 Portfolio
               </a>
             </li>
             <li className="nav-item ">
               <Link to="/contact">
-              <button className="btn rounded-pill  css-portfolio-link-blue">
+              <button className="btn rounded-pill  css-portfolio-link-blue" onClick={handleLinkClick}>
                 Contact
               </button>
               </Link>
